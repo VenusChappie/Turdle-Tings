6425669edc1c6e68b895194934548703fd102a1d
Afro_dite_codes <venuschappie@gmail.com>
2022-12-29 16:49:49 +0200

    New Functions But Nothing Works Yet

---
+++ Desktop/That Coding Stuff/Turdle_Tings/Turdle_Tings.py
@@ -0,0 +1,24 @@
+import sys
+import random
+
+def get_user_input(shapes, patterns):
+    """
+    Will ask the user for input as to what shape or pattern they would like.
+    param: shape: This is the list of valid shapes.
+    param: patterns: A list of valid patterns that can be created.
+    return: It will return a valid response from the user in Title Case.
+    """
+
+def valid_shapes():
+    """
+    Defines a list of acceptable shapes and patterns that the turtle can draw.
+    Return: List: it should return a list of those shapes and patterns.
+    """
+    shapes = ["Square", "Circle", "Rectangle", "Pentagon"]
+    return shapes
+
+def draw_tings():
+
+
+if __name__=="__main__":
+    draw_tings()
